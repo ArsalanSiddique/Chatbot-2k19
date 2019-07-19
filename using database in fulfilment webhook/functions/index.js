766a40ba9d8ad11a61c9898ddea1f907efe4505b
arsalan.ahmed1264@gmail.com <arsalan.ahmed1264@gmail.com>
2019-07-19 11:55:52 +0500

    learning webhook with database

--- using database in fulfilment webhook/functions/index.js
+++ using database in fulfilment webhook/functions/index.js
@@ -10,25 +10,63 @@ var firestore = admin.firestore();
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 
-
 exports.webhook = functions.https.onRequest((request, response) => {
 
-    console.log("request.body.result.parameters: ", request.body.queryResult.parameters);
+    switch (request.body.queryResult.action) {
+        case 'bookRoom':
+            console.log("request.body.result.parameters: ", request.body.queryResult.parameters);
 
-    let params = request.body.queryResult.parameters;
-    firestore.collection("orders").add(params)
-        .then(()=> {
-            response.send({
-                fulfillmentText:
-                `${params.name} your hotel booking request for ${params.persons} persons, we will contact you on ${params.email} soon - type of room: ${params.roomType}`
-            });
+            let params = request.body.queryResult.parameters;
+            firestore.collection("orders").add(params)
+                .then(() => {
+                    response.send({
+                        fulfillmentText:
+                            `${params.name} your hotel booking request for ${params.persons} persons, we will contact you on ${params.email} soon - type of room: ${params.roomType}`
+                    });
 
-            return res.status(200);
-        })
-        .catch((e)=> {
+                    return res.status(200);
+                })
+                .catch((e) => {
+                    response.send({
+                        fulfillmentText: "Something went wrong when writing in database."
+                    });
+                });
+            break;
+
+        case 'showBooking':
+            firestore.collection('orders').get()
+                .then((querySnapshot) => {
+
+                    var orders = [];
+                    querySnapshot.forEach((doc) => { orders.push(doc.data()) });
+
+                    var speech = `You have ${params.length} orders \n`;
+
+                    orders.forEach((eachOrder, index) => {
+                        speech += `number ${index + 1} is ${params.roomType} room for ${params.persons} persons, ordered by ${params.name} and contact email is ${params.email} \n`;
+                    })
+
+                    response.send({
+                        fulfillmentText: speech
+                    });
+                    return res.status(200);
+                })
+                .catch((err) => {
+                    console.log('Error in reading data from database.', err);
+                    response.send({
+                        fulfillmentText: "Something went wrong while reading data."
+                    })
+                })
+
+            break;
+
+        default:
             response.send({
-                fulfillmentText: "Something went wrong when writing in database."
+                fulfillmentText: 'No action matched in webHook.'
             });
-        });
+    }
+
+
+
 
 });
